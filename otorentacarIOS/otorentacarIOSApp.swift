c534cbe0d548f185b5faa2325a9b08098ec79c26
molmezses <mustafaolmezses@gmail.com>
2026-04-08 09:10:01 +0300

    project setup

---
+++ otorentacarIOS/otorentacarIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  otorentacarIOSApp.swift
+//  otorentacarIOS
+//
+//  Created by mustafaolmezses on 8.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct otorentacarIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
